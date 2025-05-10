a00a5e5f7a7c631851231ddc7efde6cb5029ff7a
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-05-10 22:34:07 +0200

    style: added prettier

--- guest-js/index.ts
+++ guest-js/index.ts
@@ -1,18 +1,35 @@
-import { PublicKeyCredential, PublicKeyCredentialCreationOptions, PublicKeyCredentialRequestOptions, RegistrationResponseJSON } from "@simplewebauthn/types";
-import { invoke } from "@tauri-apps/api/core";
+import {
+  PublicKeyCredential,
+  PublicKeyCredentialCreationOptions,
+  PublicKeyCredentialRequestOptions,
+  RegistrationResponseJSON
+} from '@simplewebauthn/types';
+import { invoke } from '@tauri-apps/api/core';
 
-export * as types from "@simplewebauthn/types";
+export * as types from '@simplewebauthn/types';
 
-export async function register(origin: string, options: PublicKeyCredentialCreationOptions): Promise<RegistrationResponseJSON | null> {
-  return await invoke<{ value?: RegistrationResponseJSON }>("plugin:webauthn|register", {
-    origin,
-    options,
-  }).then((r) => (r.value ? r.value : null));
+export async function register(
+  origin: string,
+  options: PublicKeyCredentialCreationOptions
+): Promise<RegistrationResponseJSON | null> {
+  return await invoke<{ value?: RegistrationResponseJSON }>(
+    'plugin:webauthn|register',
+    {
+      origin,
+      options
+    }
+  ).then((r) => (r.value ? r.value : null));
 }
 
-export async function authenticate(origin: string, options: PublicKeyCredentialRequestOptions): Promise<PublicKeyCredential | null> {
-  return await invoke<{ value?: PublicKeyCredential }>("plugin:webauthn|authenticate", {
-    origin,
-    options,
-  }).then((r) => (r.value ? r.value : null));
+export async function authenticate(
+  origin: string,
+  options: PublicKeyCredentialRequestOptions
+): Promise<PublicKeyCredential | null> {
+  return await invoke<{ value?: PublicKeyCredential }>(
+    'plugin:webauthn|authenticate',
+    {
+      origin,
+      options
+    }
+  ).then((r) => (r.value ? r.value : null));
 }
