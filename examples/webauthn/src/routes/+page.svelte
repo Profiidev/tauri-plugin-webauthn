5bb509a58fa78c46960c6c0bb9eac035bcc368ad
GitHub Action <action@github.com>
2025-05-13 19:14:01 +0000

    Fix code style issues with Prettier

--- examples/webauthn/src/routes/+page.svelte
+++ examples/webauthn/src/routes/+page.svelte
@@ -25,7 +25,10 @@
     );
 
     status = 'Registration options received, now calling register()...';
-    let response = await register('https://tauri-plugin-webauthn-example.glitch.me/', options);
+    let response = await register(
+      'https://tauri-plugin-webauthn-example.glitch.me/',
+      options
+    );
 
     status = 'Registration response received, now calling verification...';
     await invoke('reg_finish', { name, response });
@@ -41,7 +44,10 @@
     );
 
     status = 'Authentication options received, now calling authenticate()...';
-    let response = await authenticate('https://tauri-plugin-webauthn-example.glitch.me/', options);
+    let response = await authenticate(
+      'https://tauri-plugin-webauthn-example.glitch.me/',
+      options
+    );
 
     status = 'Authentication response received, now calling verification...';
     await invoke('auth_finish', { name, response });
